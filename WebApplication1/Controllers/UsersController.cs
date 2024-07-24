654cb594e636f097024f73633e53b8e509af8c41
alejandro tauste romero <alejandrotauste@gmail.com>
2024-07-24 18:32:24 +0200

    Juntar todos los metodo en el user controller y nueva pestaña en nav para la prueba en layout

--- WebApplication1/Controllers/UsersController.cs
+++ WebApplication1/Controllers/UsersController.cs
@@ -7,27 +7,38 @@ using WebApplication1.Service;
 
 [ApiController]
 [Route("api/[controller]")]
-public class UsersController : ControllerBase
+public class UsersController : Controller
 {
 	private readonly IApiService _apiService;
-	private readonly DbptContext _dbptContext;
+	private readonly DbptContext _context;
 
 	/// <summary>
 	/// Constructor
 	/// </summary>
 	/// <param name="apiService"></param>
 	/// <param name="dbptContext"></param>
-	public UsersController(IApiService apiService, DbptContext dbptContext)
+	public UsersController(IApiService apiService, DbptContext context)
 	{
 		_apiService = apiService;
-		_dbptContext = dbptContext;
+		_context = context;
 	}
 
-	/// <summary>
-	/// Para llamar al metodo introducir la url 'https://localhost:44353/api/users/fetch-and-insert'
-	/// </summary>
-	/// <returns></returns>
-	[HttpGet("fetch-and-insert")]
+    public async Task<IActionResult> Index()
+    {
+        //List<Usuario> users = await _context.Usuarios.ToListAsync();
+        var users = await _context.Usuarios
+            .Include(x => x.Direccion)
+            .Include(x => x.Compania)
+            .Include(x => x.Direccion.Geo)
+            .ToListAsync();
+        return View(users);
+    }
+
+    /// <summary>
+    /// Para llamar al metodo introducir la url 'https://localhost:44353/api/users/fetch-and-insert'
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("fetch-and-insert")]
 	public async Task<IActionResult> FetchAndInsertUsers()
 	{
 		string apiUrl = "https://jsonplaceholder.typicode.com/users"; 
@@ -36,38 +47,79 @@ public class UsersController : ControllerBase
 		foreach (var user in users)
 		{
 			//Identifica usuarios con Id
-			if ( _dbptContext.Usuarios.Find(user.Id) == null) 
-			{ 
-				_dbptContext.Usuarios.Add(user);
+			if (_context.Usuarios.Find(user.Id) == null) 
+			{
+                _context.Usuarios.Add(user);
 			}
 
 			if (user.Direccion != null)
 			{
-				_dbptContext.Direccions.Add(user.Direccion);
+                _context.Direccions.Add(user.Direccion);
 			}
 
 			if (user.Direccion.Geo != null)
 			{
-				//user.Direccion.Geo.DireccionId = user.Direccion.UserId;
-				//user.Direccion.Geo.Direccion = user.Direccion;
+                //user.Direccion.Geo.DireccionId = user.Direccion.UserId;
+                //user.Direccion.Geo.Direccion = user.Direccion;
 
-				_dbptContext.Geos.Add(user.Direccion.Geo);
+                _context.Geos.Add(user.Direccion.Geo);
 			}
 
 			if (user.Compania != null)
 			{
-				//user.Compania.UserId = user.Id; // Establecer la relación
-				//user.Compania.User = user;
+                //user.Compania.UserId = user.Id; // Establecer la relación
+                //user.Compania.User = user;
 
-				_dbptContext.Compania.Add(user.Compania);
+                _context.Compania.Add(user.Compania);
 			}
 		}
 
 		//Añade a la BBDD
-		await _dbptContext.SaveChangesAsync();
+		await _context.SaveChangesAsync();
 
-		return Ok(users); 
-	}
+		return Ok(users);
+    }
+
+    //[HttpPost("api/user/{id}/updateName")]
+    //public IActionResult UpdateName(int id, [FromBody] Usuario user)
+    //{
+    //    if (user == null || id != user.Id)
+    //    {
+    //        return BadRequest();
+    //    }
+
+    //    var existingUser = _context.Usuarios.Find(id);
+    //    if (existingUser == null)
+    //    {
+    //        return NotFound();
+    //    }
+
+    //    existingUser.Name = user.Name;
+    //    _context.SaveChanges();
+
+    //    return Ok();
+    //}
+
+    [HttpPost("{id}/updateName")]
+    public IActionResult UpdateName(int id, [FromBody] Usuario user)
+    {
+        if (user == null || id != user.Id)
+        {
+            return BadRequest();
+        }
+
+        var existingUser = _context.Usuarios.Find(id);
+        if (existingUser == null)
+        {
+            return NotFound();
+        }
+
+        existingUser.Name = user.Name;
+        _context.SaveChanges();
+
+        return Ok();
+    }
+
 
 }
 
